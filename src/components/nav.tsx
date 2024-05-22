1ddd28bd83e756c79e23bb2bd9e5426174ede8aa
AnujSsStw <anujzzztw@gmail.com>
2024-05-23 00:46:53 +0530

    is env is visible

---
+++ src/components/nav.tsx
@@ -0,0 +1,65 @@
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+export async function Nav() {
+  const session = await auth();
+
+  return (
+    <div className="py-2 mx-5">
+      <div className="container flex justify-between items-center">
+        <div className="flex items-center gap-12">
+          <Link href="/" className="hover:underline flex items-center gap-1">
+            <Image src="/logo.jpeg" width="50" height="50" alt="Logo" />
+          </Link>
+
+          <div className="flex items-center gap-8">
+            {session?.user && (
+              <>
+                <Link
+                  href="/items/create"
+                  className="hover:underline flex items-center gap-1"
+                >
+                  Create ➕
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          {session?.user?.image && (
+            <Image
+              src={session.user.image}
+              width="40"
+              height="40"
+              alt="user avatar"
+              className="rounded-full"
+            />
+          )}
+          <div>{session?.user?.name}</div>
+          <div>
+            {session?.user ? (
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
+            ) : (
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("github", { redirectTo: "/dashboard" });
+                }}
+              >
+                <button type="submit">Sign in</button>
+              </form>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.string().min(1),
+    UPLOADTHING_SECRET: z.string().min(1),
     // OPEN_AI_API_KEY: z.string().min(1),
   },
   client: {},
@@ -12,6 +13,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
     // OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
